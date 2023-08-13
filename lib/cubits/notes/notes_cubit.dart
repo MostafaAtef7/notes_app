3d4db7ac7aa38f7c93ba91e1b2285ffdc8f1ad65
Mostafa Atef <mostafaatef908070@gmail.com>
2023-08-13 16:25:09 +0300

    implement fetch all notes to get all notes

--- lib/cubits/notes/notes_cubit.dart
+++ lib/cubits/notes/notes_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app_12/models/note_model.dart';
+
+import '../../constants.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  void fetchAllnotes() {
+emit(NotesLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNoteBox);
+      //get list of all notes in the box
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes: notes));
+    } catch (e) {
+      emit(NotesFailure(errMessage: "Note didn't add, please try again"));
+    }
+  }
 }
--- lib/screens/main_view.dart
+++ lib/screens/main_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app_12/cubits/notes/notes_cubit.dart';
 
 import 'note_bottom_sheet_view.dart';
 
@@ -10,39 +12,42 @@ class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //display bottom sheet that float on scaffold
-          showModalBottomSheet(
-              // isScrollControlled => make bottom sheet move to dynamic height
-              isScrollControlled: true,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20)),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //display bottom sheet that float on scaffold
+            showModalBottomSheet(
+                // isScrollControlled => make bottom sheet move to dynamic height
+                isScrollControlled: true,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                ),
+                context: context,
+                builder: (context) {
+                  /*there is no build method in this area so if we return widget 
+                      and want to edit it we must restart the app so best way to 
+                      return StatelessWidget class */
+                  return const AddNote();
+                });
+          },
+          backgroundColor: Colors.blue,
+          child: const Icon(Icons.add),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              NoteAppbar(
+                title: "NOTES",
+                icon: Icon(Icons.check),
               ),
-              context: context,
-              builder: (context) {
-                /*there is no build method in this area so if we return widget 
-                  and want to edit it we must restart the app so best way to 
-                  return StatelessWidget class */
-                return const AddNote();
-              });
-        },
-        backgroundColor: Colors.blue,
-        child: const Icon(Icons.add),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            NoteAppbar(
-              title: "NOTES",
-              icon: Icon(Icons.check),
-            ),
-            ItemList(),
-          ],
+              ItemList(),
+            ],
+          ),
         ),
       ),
     );
